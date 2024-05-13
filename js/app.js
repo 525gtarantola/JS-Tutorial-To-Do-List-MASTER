29e1d14d68f4cf5748c50cb8a30903a5310a5784
525gtarantola <525gtarantola@frhsd.com>
2024-05-13 10:20:36 -0400

    InsertAdjacentHTML()

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 // CODE EXPLAINED channel
+const list = document.getElementById("list");
+
+function addToDo(toDo) {
+    const text = `<li class="item">
+                  <i class="co fa fa-circle-thin" job="complete"></i>
+                  <p class="text"> ${toDo} </p>
+                  <i class="de fa fa-trash-o" job="delete"</i>
+            </li>`
+
+    const positon = "beforeend"; 
+
+    list.insertAdjacentHTML(position, text);
+}
+
+addToDo("Drink Coffee");
