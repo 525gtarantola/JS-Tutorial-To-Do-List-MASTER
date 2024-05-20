90711a224bd7175e6e349ba1e8faf0153e701549
525gtarantola <144464759+525gtarantola@users.noreply.github.com>
2024-05-20 03:32:19 +0000

    Done, competely forgot about the commits requirement!!!

--- js/app.js
+++ js/app.js
@@ -1,67 +1,94 @@
-const clear = document.querySelector('clear')
-const dateEliment = document.getElementById('date')
-const list = document.getElementById('list')
-const input = document.getElementById('input')
-
-//classes names    - for controlling the icons when done with to do
-const CHECK = 'fa-check-circle'
-const UNCHECK = 'fa-circle-thin'
-const LINE_THROUGH = 'lineThrough'
-
-// variables
-let LIST, id;
-
+// CODE EXPLAINED channel
+//elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+//names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
 //date
-
-//object options
 const options = {
-    weekday: 'long',
-    month: 'short',
-    day:'numeric'
- }
-const today = new Date()
+    weekday: "long",
+    month: "short",
+    day: "numeric"
+  };
+  const today = new Date();
+  dateElement.innerHTML = today.toLocaleDateString("en-us", options);
+  //to do function
+  function addToDo(toDo, id, done, trash) {
+    if (trash) {
+      return;
+    }
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+    const item = `<li class="item">
+                  <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                  <p class="text ${LINE}">${toDo}</p>
+                  <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>
+                  `;
 
-dateEliment.innerHTML = today.toLocaleDateString('en-US', options)
-
-// to do function
-function addToDo(toDo, id, done, trash){
-
-    if(trash){return}
-
-    const DONE = done ? CHECK : UNCHECK
-    const LINE = done ? LINE_THROUGH :''
- const item = 
-    `<li class="item">
-         <i class="fa ${DONE} co" job =" complete" id ="${id}"></i>
-         <p class="text ${LINE}">${toDo}</p>
-         <i class="fa fa-trash-o de" job =" delete" id ="${id}"></i>
-     <li>`
-
-const position = 'beforeend';
-
-list.insertAdjacentHTML(position, item);
-
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+  }
+  //enter key
+let LIST, id;
+let data = localStorage.getItem("TODO");
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadList(LIST); 
+} else {
+  LIST = [];
+  id = 0;
 }
+function loadList(array) {
+  array.forEach(function(item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+}
+clear.addEventListener("click", function(){
+  localStorage.clear;
+  location.reload();
+});
 
-//add an item to the list using the enter key
-document.addEventListener('keyup', function(even){
-    if(event.keyCode == 13){
-        const toDO = input.value
 
-        //if input isn't empty
-        if(toDo){
-            addToDo(toDo, id, false, false);
-
-
-            LIST.push({
-                name: toDo,
-                id : id,
-                done : false,
-                trash : false
-
-            })
-            id++
-        }
-        input.value ='' 
-        }
-    })
+document.addEventListener("keyup", function(event) {
+  if (event.keyCode == 13) {
+    const toDo = input.value;
+    if (toDo) {
+      addToDo(toDo, id, false, false);
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false
+      });
+      localStorage.setItem("TODO", JSON.stringify(LIST));
+      id++;
+    }
+    input.value = "";
+  }
+});
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash = true;
+}
+list.addEventListener("click", function(event) {
+  const element = event.target; 
+  const elementJob = element.attributes.job.value;
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
+  localStorage.setItem("TODO", JSON.stringify(LIST));
+});
