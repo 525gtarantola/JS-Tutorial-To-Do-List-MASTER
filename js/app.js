a3fea1b374880d161fa735d3fbcdd6638996bddd
525gtarantola <144464759+525gtarantola@users.noreply.github.com>
2024-05-20 02:51:55 +0000

    back on my grind

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 // CODE EXPLAINED channel
+// CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+
+const dateElement = document.getElementById("date");
+
+
 const list = document.getElementById("list");
 
-function addToDo(toDo) {
-    const text = `<li class="item">
-                  <i class="co fa fa-circle-thin" job="complete"></i>
-                  <p class="text"> ${toDo} </p>
-                  <i class="de fa fa-trash-o" job="delete"</i>
-            </li>`
 
-    const positon = "beforeend"; 
+const input = documnt.getElementById("input"); 
 
-    list.insertAdjacentHTML(position, text);
+//insertAdjacentHTML()
+const element = document.getElementById("element"); 
+// syntax: 
+
+
+// add a to-do
+function addToDo (toDo){ 
+const text = `<li class="item"> 
+<i class="co fa fa-circle-thin" job="complete"></i>
+<p class="text"> Drink Coffee </p>
+<i class="de fa fa-trash-o" job="delete"> </i>
+</li>`
+const position = "beforeend"; 
+list.insertAdjacentHTML(postion, text); 
 }
-
-addToDo("Drink Coffee");
+addToDo("Drink Coffee"); 
